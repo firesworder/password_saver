7f98b897c7944057200404bcdf3e3e1b1616162d
firesworder <cus.never.again@yandex.ru>
2023-08-26 22:42:41 +0300

    23_08_2023_plan server реализовал методы регистрации и аутент. пользователя

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,14 +1,80 @@
 package server
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
 	"github.com/firesworder/password_saver/internal/server/env"
+	"github.com/firesworder/password_saver/internal/storage"
+	"github.com/firesworder/password_saver/internal/storage/mocks/users"
 )
 
+var ErrWrongPassword = errors.New("wrong password")
+
 type Server struct {
-	env *env.Environment
+	env       *env.Environment
+	uRep      storage.UserRepository
+	authUsers map[string]storage.User
 }
 
 func NewServer() (*Server, error) {
-	s := &Server{env: &env.Env}
+	var uRep storage.UserRepository
+	// todo: удалить после разработки
+	if true {
+		uRep = &users.MockUser{Users: map[string]storage.User{}}
+	}
+	s := &Server{env: &env.Env, uRep: uRep, authUsers: map[string]storage.User{}}
 	return s, nil
+}
+
+// generateRandom - создает массив байт заданной длины
+func generateRandom(size int) ([]byte, error) {
+	randBytes := make([]byte, size)
+	_, err := rand.Read(randBytes)
+	if err != nil {
+		return nil, err
+	}
+	return randBytes, nil
+}
+
+func generateToken() ([]byte, error) {
+	return generateRandom(32)
+}
+
+func (s *Server) RegisterUser(ctx context.Context, user storage.User) error {
+	// хеширование пароля
+
+	err := s.uRep.CreateUser(ctx, user)
+	if err != nil {
+		return err
+	}
+
+	uTokenBytes, err := generateToken()
+	if err != nil {
+		return err
+	}
+	uToken := hex.EncodeToString(uTokenBytes)
+
+	s.authUsers[uToken] = user
+	return nil
+}
+
+func (s *Server) LoginUser(ctx context.Context, user storage.User) error {
+	bdUser, err := s.uRep.GetUser(ctx, user)
+	if err != nil {
+		return err
+	}
+	if bdUser.HashedPassword != user.HashedPassword {
+		return ErrWrongPassword
+	}
+
+	uTokenBytes, err := generateToken()
+	if err != nil {
+		return err
+	}
+	uToken := hex.EncodeToString(uTokenBytes)
+
+	s.authUsers[uToken] = user
+	return nil
 }
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,7 +1,11 @@
 package server
 
 import (
+	"context"
+	"github.com/firesworder/password_saver/internal/storage"
+	"github.com/firesworder/password_saver/internal/storage/mocks/users"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -22,6 +26,101 @@ func TestNewServer(t *testing.T) {
 			if err != nil {
 				assert.NotEmpty(t, s.env)
 			}
+		})
+	}
+}
+
+func TestServer_RegisterUser(t *testing.T) {
+	s, err := NewServer()
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	tests := []struct {
+		name          string
+		user          storage.User
+		uRepState     storage.UserRepository
+		wantURepState storage.UserRepository
+		wantError     bool
+	}{
+		{
+			name: "Test 1. Successful user reg",
+			user: storage.User{Login: "Ayaka", HashedPassword: "hashed_pass1"},
+			uRepState: &users.MockUser{Users: map[string]storage.User{
+				"Ayato": {Login: "Ayato", HashedPassword: "hashed_pass2"},
+			}},
+			wantURepState: &users.MockUser{Users: map[string]storage.User{
+				"Ayato": {Login: "Ayato", HashedPassword: "hashed_pass2"},
+				"Ayaka": {Login: "Ayaka", HashedPassword: "hashed_pass1"},
+			}},
+			wantError: false,
+		},
+		{
+			name: "Test 2. Error user reg, user exists",
+			user: storage.User{Login: "Ayato", HashedPassword: "hashed_pass1"},
+			uRepState: &users.MockUser{Users: map[string]storage.User{
+				"Ayato": {Login: "Ayato", HashedPassword: "hashed_pass2"},
+			}},
+			wantURepState: &users.MockUser{Users: map[string]storage.User{
+				"Ayato": {Login: "Ayato", HashedPassword: "hashed_pass2"},
+			}},
+			wantError: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s.uRep = tt.uRepState
+
+			gotErr := s.RegisterUser(ctx, tt.user)
+			assert.Equal(t, tt.wantError, gotErr != nil)
+			assert.Equal(t, tt.wantURepState, s.uRep)
+		})
+	}
+}
+
+func TestServer_LoginUser(t *testing.T) {
+	s, err := NewServer()
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	tests := []struct {
+		name      string
+		user      storage.User
+		uRepState storage.UserRepository
+		wantError bool
+	}{
+		{
+			name: "Test 1. Successful user login",
+			user: storage.User{Login: "Ayato", HashedPassword: "hashed_pass2"},
+			uRepState: &users.MockUser{Users: map[string]storage.User{
+				"Ayato": {Login: "Ayato", HashedPassword: "hashed_pass2"},
+				"Ayaka": {Login: "Ayaka", HashedPassword: "hashed_pass1"},
+			}},
+			wantError: false,
+		},
+		{
+			name: "Test 2. Error user log, user not exists",
+			user: storage.User{Login: "Ayaka", HashedPassword: "hashed_pass1"},
+			uRepState: &users.MockUser{Users: map[string]storage.User{
+				"Ayato": {Login: "Ayato", HashedPassword: "hashed_pass2"},
+			}},
+			wantError: true,
+		},
+		{
+			name: "Test 2. Error user log, user pass not correct",
+			user: storage.User{Login: "Ayaka", HashedPassword: "random_pass"},
+			uRepState: &users.MockUser{Users: map[string]storage.User{
+				"Ayato": {Login: "Ayato", HashedPassword: "hashed_pass2"},
+				"Ayaka": {Login: "Ayaka", HashedPassword: "hashed_pass1"},
+			}},
+			wantError: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s.uRep = tt.uRepState
+
+			gotErr := s.LoginUser(ctx, tt.user)
+			assert.Equal(t, tt.wantError, gotErr != nil)
 		})
 	}
 }
