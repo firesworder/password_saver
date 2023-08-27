469e82b6694e97c56f0ac7d7a233b1dd140ac4ff
firesworder <cus.never.again@yandex.ru>
2023-08-27 06:13:19 +0300

    23_08_2023_plan Реализовал обработку данных пользователя на сервере

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,6 +7,9 @@ import (
 	"errors"
 	"github.com/firesworder/password_saver/internal/server/env"
 	"github.com/firesworder/password_saver/internal/storage"
+	"github.com/firesworder/password_saver/internal/storage/mocks/bankdata"
+	"github.com/firesworder/password_saver/internal/storage/mocks/binarydata"
+	"github.com/firesworder/password_saver/internal/storage/mocks/textdata"
 	"github.com/firesworder/password_saver/internal/storage/mocks/users"
 )
 
@@ -16,15 +19,31 @@ type Server struct {
 	env       *env.Environment
 	uRep      storage.UserRepository
 	authUsers map[string]storage.User
+	tRep      storage.TextDataRepository
+	bankRep   storage.BankDataRepository
+	binRep    storage.BinaryDataRepository
 }
 
 func NewServer() (*Server, error) {
 	var uRep storage.UserRepository
+	var tRep storage.TextDataRepository
+	var bankRep storage.BankDataRepository
+	var binRep storage.BinaryDataRepository
 	// todo: удалить после разработки
 	if true {
 		uRep = &users.MockUser{Users: map[string]storage.User{}}
+		tRep = &textdata.MockTextData{TextDataMap: map[int]storage.TextData{}}
+		bankRep = &bankdata.MockBankData{BankData: map[int]storage.BankData{}}
+		binRep = &binarydata.MockBinaryData{BinaryData: map[int]storage.BinaryData{}}
 	}
-	s := &Server{env: &env.Env, uRep: uRep, authUsers: map[string]storage.User{}}
+	s := &Server{
+		env:       &env.Env,
+		uRep:      uRep,
+		tRep:      tRep,
+		bankRep:   bankRep,
+		binRep:    binRep,
+		authUsers: map[string]storage.User{},
+	}
 	return s, nil
 }
 
@@ -76,5 +95,77 @@ func (s *Server) LoginUser(ctx context.Context, user storage.User) error {
 	uToken := hex.EncodeToString(uTokenBytes)
 
 	s.authUsers[uToken] = user
+	return nil
+}
+
+func (s *Server) AddTextData(ctx context.Context, textData storage.TextData) error {
+	err := s.tRep.AddTextData(ctx, textData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) UpdateTextData(ctx context.Context, textData storage.TextData) error {
+	err := s.tRep.UpdateTextData(ctx, textData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) DeleteTextData(ctx context.Context, textData storage.TextData) error {
+	err := s.tRep.DeleteTextData(ctx, textData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) AddBankData(ctx context.Context, bankData storage.BankData) error {
+	err := s.bankRep.AddBankData(ctx, bankData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) UpdateBankData(ctx context.Context, bankData storage.BankData) error {
+	err := s.bankRep.UpdateBankData(ctx, bankData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) DeleteBankData(ctx context.Context, bankData storage.BankData) error {
+	err := s.bankRep.DeleteBankData(ctx, bankData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) AddBinaryData(ctx context.Context, binaryData storage.BinaryData) error {
+	err := s.binRep.AddBinaryData(ctx, binaryData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) UpdateBinaryData(ctx context.Context, binaryData storage.BinaryData) error {
+	err := s.binRep.UpdateBinaryData(ctx, binaryData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) DeleteBinaryData(ctx context.Context, binaryData storage.BinaryData) error {
+	err := s.binRep.DeleteBinaryData(ctx, binaryData)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -30,6 +30,20 @@ func TestNewServer(t *testing.T) {
 	}
 }
 
+func Test_generateRandom(t *testing.T) {
+	bytesLen := 32
+
+	randBytes, err := generateRandom(bytesLen)
+	require.NoError(t, err)
+	assert.Equal(t, bytesLen, len(randBytes))
+}
+
+func Test_generateToken(t *testing.T) {
+	token, err := generateToken()
+	require.NoError(t, err)
+	assert.Equal(t, 32, len(token))
+}
+
 func TestServer_RegisterUser(t *testing.T) {
 	s, err := NewServer()
 	require.NoError(t, err)
@@ -123,4 +137,135 @@ func TestServer_LoginUser(t *testing.T) {
 			assert.Equal(t, tt.wantError, gotErr != nil)
 		})
 	}
+}
+
+func TestServer_AddTextData(t *testing.T) {
+	ctx := context.Background()
+	s, err := NewServer()
+	require.NoError(t, err)
+
+	td := storage.TextData{TextData: "I will...Neeko!"}
+	err = s.AddTextData(ctx, td)
+	require.NoError(t, err)
+}
+
+func TestServer_UpdateTextData(t *testing.T) {
+	ctx := context.Background()
+	s, err := NewServer()
+	require.NoError(t, err)
+
+	td := storage.TextData{TextData: "I will...Neeko!"}
+	err = s.AddTextData(ctx, td)
+	require.NoError(t, err)
+
+	tdUpdate := storage.TextData{ID: 1, TextData: "Definitely not Neeko!"}
+	err = s.UpdateTextData(ctx, tdUpdate)
+	require.NoError(t, err)
+}
+
+func TestServer_DeleteTextData(t *testing.T) {
+	ctx := context.Background()
+	s, err := NewServer()
+	require.NoError(t, err)
+
+	td := storage.TextData{TextData: "I will...Neeko!"}
+	err = s.AddTextData(ctx, td)
+	require.NoError(t, err)
+
+	tdToDelete := storage.TextData{ID: 1}
+	err = s.DeleteTextData(ctx, tdToDelete)
+	require.NoError(t, err)
+}
+
+func TestServer_AddBankData(t *testing.T) {
+	ctx := context.Background()
+	s, err := NewServer()
+	require.NoError(t, err)
+
+	bankD := storage.BankData{
+		CardNumber: "0011 2233 4455 6677",
+		CardExpire: "88/00",
+		CVV:        "456",
+	}
+	err = s.AddBankData(ctx, bankD)
+	require.NoError(t, err)
+}
+
+func TestServer_UpdateBankData(t *testing.T) {
+	ctx := context.Background()
+	s, err := NewServer()
+	require.NoError(t, err)
+
+	bankD := storage.BankData{
+		CardNumber: "0011 2233 4455 6677",
+		CardExpire: "88/00",
+		CVV:        "456",
+	}
+	err = s.AddBankData(ctx, bankD)
+	require.NoError(t, err)
+
+	bankDUpdate := storage.BankData{
+		ID:         1,
+		CardNumber: "8800 9900 7700 6666",
+		CardExpire: "88/00",
+		CVV:        "456",
+	}
+	err = s.UpdateBankData(ctx, bankDUpdate)
+	require.NoError(t, err)
+}
+
+func TestServer_DeleteBankData(t *testing.T) {
+	ctx := context.Background()
+	s, err := NewServer()
+	require.NoError(t, err)
+
+	bankD := storage.BankData{
+		CardNumber: "0011 2233 4455 6677",
+		CardExpire: "88/00",
+		CVV:        "456",
+	}
+	err = s.AddBankData(ctx, bankD)
+	require.NoError(t, err)
+
+	bankDDelete := storage.BankData{ID: 1}
+	err = s.DeleteBankData(ctx, bankDDelete)
+	require.NoError(t, err)
+}
+
+func TestServer_AddBinaryData(t *testing.T) {
+	ctx := context.Background()
+	s, err := NewServer()
+	require.NoError(t, err)
+
+	binD := storage.BinaryData{BinaryData: []byte("Bayonetta")}
+	err = s.AddBinaryData(ctx, binD)
+	require.NoError(t, err)
+}
+
+func TestServer_UpdateBinaryData(t *testing.T) {
+	ctx := context.Background()
+	s, err := NewServer()
+	require.NoError(t, err)
+
+	binD := storage.BinaryData{BinaryData: []byte("Bayonetta")}
+	err = s.AddBinaryData(ctx, binD)
+	require.NoError(t, err)
+
+	binDUpdate := storage.BinaryData{ID: 1, BinaryData: []byte("Cipher")}
+	err = s.UpdateBinaryData(ctx, binDUpdate)
+	require.NoError(t, err)
+}
+
+func TestServer_DeleteBinaryData(t *testing.T) {
+	ctx := context.Background()
+	s, err := NewServer()
+	require.NoError(t, err)
+
+	binD := storage.BinaryData{BinaryData: []byte("Bayonetta")}
+	err = s.AddBinaryData(ctx, binD)
+	require.NoError(t, err)
+
+	binDDelete := storage.BinaryData{ID: 1}
+	err = s.DeleteBinaryData(ctx, binDDelete)
+	require.NoError(t, err)
 }
