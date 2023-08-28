c15219cd0e25ea2597b91b9dbd844d3adcb7219e
firesworder <cus.never.again@yandex.ru>
2023-08-29 00:32:16 +0300

    28_08_2023_plan Реализовал grpc сервер

--- internal/grpcserver/grpcserver.go
+++ internal/grpcserver/grpcserver.go
@@ -52,51 +52,165 @@ func (gs *GRPCServer) LoginUser(ctx context.Context, request *pb.LoginUserReques
 }
 
 func (gs *GRPCServer) AddTextDataRecord(ctx context.Context, request *pb.AddTextDataRequest) (*pb.AddTextDataResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := gs.serv.AddTextData(ctx, storage.TextData{
+		TextData: request.TextData.TextData,
+		MetaInfo: request.TextData.MetaInfo,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.AddTextDataResponse{}
+	return resp, nil
 }
 
 func (gs *GRPCServer) UpdateTextDataRecord(ctx context.Context, request *pb.UpdateTextDataRequest) (*pb.UpdateTextDataResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := gs.serv.UpdateTextData(ctx, storage.TextData{
+		TextData: request.TextData.TextData,
+		MetaInfo: request.TextData.MetaInfo,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.UpdateTextDataResponse{}
+	return resp, nil
 }
 
 func (gs *GRPCServer) DeleteTextDataRecord(ctx context.Context, request *pb.DeleteTextDataRequest) (*pb.DeleteTextDataResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := gs.serv.DeleteTextData(ctx, storage.TextData{
+		ID: int(request.Id),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.DeleteTextDataResponse{}
+	return resp, nil
 }
 
 func (gs *GRPCServer) AddBankDataRecord(ctx context.Context, request *pb.AddBankDataRequest) (*pb.AddBankDataResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := gs.serv.AddBankData(ctx, storage.BankData{
+		CardNumber: request.BankData.CardNumber,
+		CardExpire: request.BankData.CardExpiry,
+		CVV:        request.BankData.Cvv,
+		MetaInfo:   request.BankData.MetaInfo,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.AddBankDataResponse{}
+	return resp, nil
 }
 
 func (gs *GRPCServer) UpdateBankDataRecord(ctx context.Context, request *pb.UpdateBankDataRequest) (*pb.UpdateBankDataResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := gs.serv.UpdateBankData(ctx, storage.BankData{
+		ID:         int(request.BankData.Id),
+		CardNumber: request.BankData.CardNumber,
+		CardExpire: request.BankData.CardExpiry,
+		CVV:        request.BankData.Cvv,
+		MetaInfo:   request.BankData.MetaInfo,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.UpdateBankDataResponse{}
+	return resp, nil
 }
 
 func (gs *GRPCServer) DeleteBankDataRecord(ctx context.Context, request *pb.DeleteBankDataRequest) (*pb.DeleteBankDataResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := gs.serv.DeleteBankData(ctx, storage.BankData{
+		ID: int(request.Id),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.DeleteBankDataResponse{}
+	return resp, nil
 }
 
 func (gs *GRPCServer) AddBinaryDataRecord(ctx context.Context, request *pb.AddBinaryDataRequest) (*pb.AddBinaryDataResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := gs.serv.AddBinaryData(ctx, storage.BinaryData{
+		BinaryData: request.BinaryData.BinaryData,
+		MetaInfo:   request.BinaryData.MetaInfo,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.AddBinaryDataResponse{}
+	return resp, nil
 }
 
 func (gs *GRPCServer) UpdateBinaryDataRecord(ctx context.Context, request *pb.UpdateBinaryDataRequest) (*pb.UpdateBinaryDataResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := gs.serv.UpdateBinaryData(ctx, storage.BinaryData{
+		ID:         int(request.BinaryData.Id),
+		BinaryData: request.BinaryData.BinaryData,
+		MetaInfo:   request.BinaryData.MetaInfo,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.UpdateBinaryDataResponse{}
+	return resp, nil
 }
 
 func (gs *GRPCServer) DeleteBinaryDataRecord(ctx context.Context, request *pb.DeleteBinaryDataRequest) (*pb.DeleteBinaryDataResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := gs.serv.DeleteBinaryData(ctx, storage.BinaryData{
+		ID: int(request.Id),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.DeleteBinaryDataResponse{}
+	return resp, nil
 }
 
 func (gs *GRPCServer) GetAllRecords(ctx context.Context, request *pb.GetAllRecordsRequest) (*pb.GetAllRecordsResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	records, err := gs.serv.GetAllRecords(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	textDL := make([]*pb.TextData, 0, len(records.TextDataList))
+	bankDL := make([]*pb.BankData, 0, len(records.BankDataList))
+	binaryDL := make([]*pb.BinaryData, 0, len(records.BinaryDataList))
+
+	for _, v := range records.TextDataList {
+		textDL = append(textDL, &pb.TextData{
+			Id:       int64(v.ID),
+			TextData: v.TextData,
+			MetaInfo: v.MetaInfo,
+		})
+	}
+
+	for _, v := range records.BankDataList {
+		bankDL = append(bankDL, &pb.BankData{
+			Id:         int64(v.ID),
+			CardNumber: v.CardNumber,
+			CardExpiry: v.CardExpire,
+			Cvv:        v.CVV,
+			MetaInfo:   v.MetaInfo,
+		})
+	}
+
+	for _, v := range records.BinaryDataList {
+		binaryDL = append(binaryDL, &pb.BinaryData{
+			Id:         int64(v.ID),
+			BinaryData: v.BinaryData,
+			MetaInfo:   v.MetaInfo,
+		})
+	}
+
+	resp := &pb.GetAllRecordsResponse{
+		TextDataList:   textDL,
+		BankDataList:   bankDL,
+		BinaryDataList: binaryDL,
+	}
+	return resp, nil
 }
--- internal/grpcserver/grpcserver_test.go
+++ internal/grpcserver/grpcserver_test.go
@@ -3,6 +3,9 @@ package grpcserver
 import (
 	"context"
 	"github.com/firesworder/password_saver/internal/server"
+	"github.com/firesworder/password_saver/internal/storage/mocks/bankdata"
+	"github.com/firesworder/password_saver/internal/storage/mocks/binarydata"
+	"github.com/firesworder/password_saver/internal/storage/mocks/textdata"
 	"github.com/firesworder/password_saver/internal/storage/mocks/users"
 	pb "github.com/firesworder/password_saver/proto"
 	"github.com/stretchr/testify/assert"
@@ -91,6 +94,341 @@ func TestGRPCServer_LoginUser(t *testing.T) {
 			grpcS.serv = s
 
 			gotResp, gotErr := grpcS.LoginUser(ctx, tt.req)
+			assert.Equal(t, tt.wantResp, gotResp)
+			assert.ErrorIs(t, tt.wantErr, gotErr)
+		})
+	}
+}
+
+func TestGRPCServer_AddBankDataRecord(t *testing.T) {
+	grpcS := GRPCServer{serv: nil}
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		req      *pb.AddBankDataRequest
+		wantResp *pb.AddBankDataResponse
+		wantErr  error
+	}{
+		{
+			name: "Test 1. Basic test",
+			req: &pb.AddBankDataRequest{BankData: &pb.BankData{
+				CardNumber: "0011 2233 4455 6677",
+				CardExpiry: "05/24",
+				Cvv:        "987",
+				MetaInfo:   "",
+			}},
+			wantResp: &pb.AddBankDataResponse{},
+			wantErr:  nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := server.NewServer()
+			require.NoError(t, err)
+			grpcS.serv = s
+
+			gotResp, gotErr := grpcS.AddBankDataRecord(ctx, tt.req)
+			assert.Equal(t, tt.wantResp, gotResp)
+			assert.ErrorIs(t, tt.wantErr, gotErr)
+		})
+	}
+}
+
+func TestGRPCServer_AddBinaryDataRecord(t *testing.T) {
+	grpcS := GRPCServer{serv: nil}
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		req      *pb.AddBinaryDataRequest
+		wantResp *pb.AddBinaryDataResponse
+		wantErr  error
+	}{
+		{
+			name: "Test 1. Basic test",
+			req: &pb.AddBinaryDataRequest{BinaryData: &pb.BinaryData{
+				BinaryData: []byte("Ayaka"),
+				MetaInfo:   "Ayayaka",
+			}},
+			wantResp: &pb.AddBinaryDataResponse{},
+			wantErr:  nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := server.NewServer()
+			require.NoError(t, err)
+			grpcS.serv = s
+
+			gotResp, gotErr := grpcS.AddBinaryDataRecord(ctx, tt.req)
+			assert.Equal(t, tt.wantResp, gotResp)
+			assert.ErrorIs(t, tt.wantErr, gotErr)
+		})
+	}
+}
+
+func TestGRPCServer_AddTextDataRecord(t *testing.T) {
+	grpcS := GRPCServer{serv: nil}
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		req      *pb.AddTextDataRequest
+		wantResp *pb.AddTextDataResponse
+		wantErr  error
+	}{
+		{
+			name: "Test 1. Basic test",
+			req: &pb.AddTextDataRequest{TextData: &pb.TextData{
+				TextData: "Ayato",
+				MetaInfo: "Ayayato",
+			}},
+			wantResp: &pb.AddTextDataResponse{},
+			wantErr:  nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := server.NewServer()
+			require.NoError(t, err)
+			grpcS.serv = s
+
+			gotResp, gotErr := grpcS.AddTextDataRecord(ctx, tt.req)
+			assert.Equal(t, tt.wantResp, gotResp)
+			assert.ErrorIs(t, tt.wantErr, gotErr)
+		})
+	}
+}
+
+func TestGRPCServer_UpdateBankDataRecord(t *testing.T) {
+	grpcS := GRPCServer{serv: nil}
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		req      *pb.UpdateBankDataRequest
+		wantResp *pb.UpdateBankDataResponse
+		wantErr  error
+	}{
+		{
+			name: "Test 1. Basic test",
+			req: &pb.UpdateBankDataRequest{BankData: &pb.BankData{
+				Id:         int64(1),
+				CardNumber: "0011 2233 4455 6677",
+				CardExpiry: "05/24",
+				Cvv:        "987",
+				MetaInfo:   "",
+			}},
+			wantResp: nil,
+			wantErr:  bankdata.ErrNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := server.NewServer()
+			require.NoError(t, err)
+			grpcS.serv = s
+
+			gotResp, gotErr := grpcS.UpdateBankDataRecord(ctx, tt.req)
+			assert.Equal(t, tt.wantResp, gotResp)
+			assert.ErrorIs(t, tt.wantErr, gotErr)
+		})
+	}
+}
+
+func TestGRPCServer_UpdateBinaryDataRecord(t *testing.T) {
+	grpcS := GRPCServer{serv: nil}
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		req      *pb.UpdateBinaryDataRequest
+		wantResp *pb.UpdateBinaryDataResponse
+		wantErr  error
+	}{
+		{
+			name: "Test 1. Basic test",
+			req: &pb.UpdateBinaryDataRequest{BinaryData: &pb.BinaryData{
+				Id:         int64(1),
+				BinaryData: []byte("Ayato"),
+				MetaInfo:   "",
+			}},
+			wantResp: nil,
+			wantErr:  binarydata.ErrNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := server.NewServer()
+			require.NoError(t, err)
+			grpcS.serv = s
+
+			gotResp, gotErr := grpcS.UpdateBinaryDataRecord(ctx, tt.req)
+			assert.Equal(t, tt.wantResp, gotResp)
+			assert.ErrorIs(t, tt.wantErr, gotErr)
+		})
+	}
+}
+
+func TestGRPCServer_UpdateTextDataRecord(t *testing.T) {
+	grpcS := GRPCServer{serv: nil}
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		req      *pb.UpdateTextDataRequest
+		wantResp *pb.UpdateTextDataResponse
+		wantErr  error
+	}{
+		{
+			name: "Test 1. Basic test",
+			req: &pb.UpdateTextDataRequest{TextData: &pb.TextData{
+				Id:       int64(1),
+				TextData: "Bayonetta",
+				MetaInfo: "a computer game",
+			}},
+			wantResp: nil,
+			wantErr:  textdata.ErrNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := server.NewServer()
+			require.NoError(t, err)
+			grpcS.serv = s
+
+			gotResp, gotErr := grpcS.UpdateTextDataRecord(ctx, tt.req)
+			assert.Equal(t, tt.wantResp, gotResp)
+			assert.ErrorIs(t, tt.wantErr, gotErr)
+		})
+	}
+}
+
+func TestGRPCServer_DeleteBankDataRecord(t *testing.T) {
+	grpcS := GRPCServer{serv: nil}
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		req      *pb.DeleteBankDataRequest
+		wantResp *pb.DeleteBankDataResponse
+		wantErr  error
+	}{
+		{
+			name: "Test 1. Basic test",
+			req: &pb.DeleteBankDataRequest{
+				Id: int64(1),
+			},
+			wantResp: nil,
+			wantErr:  bankdata.ErrNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := server.NewServer()
+			require.NoError(t, err)
+			grpcS.serv = s
+
+			gotResp, gotErr := grpcS.DeleteBankDataRecord(ctx, tt.req)
+			assert.Equal(t, tt.wantResp, gotResp)
+			assert.ErrorIs(t, tt.wantErr, gotErr)
+		})
+	}
+}
+
+func TestGRPCServer_DeleteBinaryDataRecord(t *testing.T) {
+	grpcS := GRPCServer{serv: nil}
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		req      *pb.DeleteBinaryDataRequest
+		wantResp *pb.DeleteBinaryDataResponse
+		wantErr  error
+	}{
+		{
+			name: "Test 1. Basic test",
+			req: &pb.DeleteBinaryDataRequest{
+				Id: int64(1),
+			},
+			wantResp: nil,
+			wantErr:  binarydata.ErrNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := server.NewServer()
+			require.NoError(t, err)
+			grpcS.serv = s
+
+			gotResp, gotErr := grpcS.DeleteBinaryDataRecord(ctx, tt.req)
+			assert.Equal(t, tt.wantResp, gotResp)
+			assert.ErrorIs(t, tt.wantErr, gotErr)
+		})
+	}
+}
+
+func TestGRPCServer_DeleteTextDataRecord(t *testing.T) {
+	grpcS := GRPCServer{serv: nil}
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		req      *pb.DeleteTextDataRequest
+		wantResp *pb.DeleteTextDataResponse
+		wantErr  error
+	}{
+		{
+			name: "Test 1. Basic test",
+			req: &pb.DeleteTextDataRequest{
+				Id: int64(1),
+			},
+			wantResp: nil,
+			wantErr:  textdata.ErrNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := server.NewServer()
+			require.NoError(t, err)
+			grpcS.serv = s
+
+			gotResp, gotErr := grpcS.DeleteTextDataRecord(ctx, tt.req)
+			assert.Equal(t, tt.wantResp, gotResp)
+			assert.ErrorIs(t, tt.wantErr, gotErr)
+		})
+	}
+}
+
+func TestGRPCServer_GetAllRecords(t *testing.T) {
+	grpcS := GRPCServer{serv: nil}
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		req      *pb.GetAllRecordsRequest
+		wantResp *pb.GetAllRecordsResponse
+		wantErr  error
+	}{
+		{
+			name: "Test 1. Basic test",
+			req:  &pb.GetAllRecordsRequest{},
+			wantResp: &pb.GetAllRecordsResponse{
+				TextDataList:   []*pb.TextData{},
+				BankDataList:   []*pb.BankData{},
+				BinaryDataList: []*pb.BinaryData{},
+			},
+			wantErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := server.NewServer()
+			require.NoError(t, err)
+			grpcS.serv = s
+
+			gotResp, gotErr := grpcS.GetAllRecords(ctx, tt.req)
 			assert.Equal(t, tt.wantResp, gotResp)
 			assert.ErrorIs(t, tt.wantErr, gotErr)
 		})
