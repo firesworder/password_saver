4d3517060434ee3e0fc77599844722731a6c83af
firesworder <cus.never.again@yandex.ru>
2023-09-04 03:07:40 +0300

    28_08_2023_plan реализовал функции запуска агента и сервера

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"github.com/firesworder/password_saver/internal/agent"
+	"log"
+)
 
+func main() {
+	a, err := agent.NewAgent()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Fatal(a.Serve())
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"github.com/firesworder/password_saver/internal/grpcserver"
+	"github.com/firesworder/password_saver/internal/server"
+	pb "github.com/firesworder/password_saver/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
+func main() {
+	s, err := server.NewServer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listen, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	grpcService, err := grpcserver.NewGRPCServer(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	serverGRPC := grpc.NewServer()
+	pb.RegisterPasswordSaverServer(serverGRPC, grpcService)
+
+	log.Fatal(serverGRPC.Serve(listen))
 }
