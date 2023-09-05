bdc94f73ad0ec4dcae2aaea8e5b6c7cc495df861
firesworder <cus.never.again@yandex.ru>
2023-09-05 08:20:02 +0300

    sqlstorage Добавил ошибку в константы

--- internal/storage/constants.go
+++ internal/storage/constants.go
@@ -7,4 +7,6 @@ const DevDSN = "postgresql://postgres:admin@localhost:5432/password_saver"
 var (
 	ErrLoginExist    = errors.New("login already exist")
 	ErrLoginNotExist = errors.New("login not exist")
+
+	ErrElementNotFound = errors.New("element was not found")
 )
