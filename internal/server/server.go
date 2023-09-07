2dff654c5bf42cf0434ce346505db0860a9a674e
firesworder <cus.never.again@yandex.ru>
2023-09-07 22:01:35 +0300

    server Рефакторинг

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,67 +2,70 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"github.com/firesworder/password_saver/internal/server/env"
 	"github.com/firesworder/password_saver/internal/storage"
-	"github.com/firesworder/password_saver/internal/storage/mocks/bankdata"
-	"github.com/firesworder/password_saver/internal/storage/mocks/binarydata"
-	"github.com/firesworder/password_saver/internal/storage/mocks/textdata"
-	"github.com/firesworder/password_saver/internal/storage/mocks/users"
 	"github.com/firesworder/password_saver/internal/storage/sqlstorage"
 )
 
+const ctxTokenParam = "userToken"
+
 type Server struct {
 	env       *env.Environment
-	uRep      storage.UserRepository
 	authUsers map[string]storage.User
-	tRep      storage.TextDataRepository
-	bankRep   storage.BankDataRepository
-	binRep    storage.BinaryDataRepository
+
+	uRep    storage.UserRepository
+	tRep    storage.TextDataRepository
+	bankRep storage.BankDataRepository
+	binRep  storage.BinaryDataRepository
 
 	genToken []byte
 }
 
 func NewServer() (*Server, error) {
-	var uRep storage.UserRepository
-	var tRep storage.TextDataRepository
-	var bankRep storage.BankDataRepository
-	var binRep storage.BinaryDataRepository
-
-	// todo: dev
 	ssql, err := sqlstorage.NewStorage(storage.DevDSN)
-	//if err != nil {
-	//	return nil, err
-	//}
 	if err != nil {
-		uRep = &users.MockUser{Users: map[string]storage.User{}}
-		tRep = &textdata.MockTextData{TextDataMap: map[int]storage.TextData{}}
-		bankRep = &bankdata.MockBankData{BankData: map[int]storage.BankData{}}
-		binRep = &binarydata.MockBinaryData{BinaryData: map[int]storage.BinaryData{}}
-	} else {
-		uRep = ssql.UserRep
-		tRep = ssql.TextRep
-		bankRep = ssql.BankRep
-		binRep = ssql.BinaryRep
+		return nil, err
 	}
 
 	genToken, err := generateRandom(32)
 	if err != nil {
 		return nil, err
 	}
+
 	s := &Server{
 		env:       &env.Env,
-		uRep:      uRep,
-		tRep:      tRep,
-		bankRep:   bankRep,
-		binRep:    binRep,
 		authUsers: map[string]storage.User{},
-		genToken:  genToken,
+
+		uRep:    ssql.UserRep,
+		tRep:    ssql.TextRep,
+		bankRep: ssql.BankRep,
+		binRep:  ssql.BinaryRep,
+
+		genToken: genToken,
 	}
 	return s, nil
 }
 
+func (s *Server) getUserFromContext(ctx context.Context) (*storage.User, error) {
+	token, ok := ctx.Value(ctxTokenParam).(string)
+	if !ok {
+		return nil, fmt.Errorf("userToken is not set")
+	}
+	user, ok := s.authUsers[token]
+	if !ok {
+		return nil, fmt.Errorf("user is not auth")
+	}
+	return &user, nil
+}
+
 func (s *Server) AddTextData(ctx context.Context, textData storage.TextData) (int, error) {
-	id, err := s.tRep.AddTextData(ctx, textData)
+	u, err := s.getUserFromContext(ctx)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := s.tRep.AddTextData(ctx, textData, u)
 	if err != nil {
 		return 0, err
 	}
@@ -70,7 +73,12 @@ func (s *Server) AddTextData(ctx context.Context, textData storage.TextData) (in
 }
 
 func (s *Server) UpdateTextData(ctx context.Context, textData storage.TextData) error {
-	err := s.tRep.UpdateTextData(ctx, textData)
+	u, err := s.getUserFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = s.tRep.UpdateTextData(ctx, textData, u)
 	if err != nil {
 		return err
 	}
@@ -78,7 +86,12 @@ func (s *Server) UpdateTextData(ctx context.Context, textData storage.TextData) 
 }
 
 func (s *Server) DeleteTextData(ctx context.Context, textData storage.TextData) error {
-	err := s.tRep.DeleteTextData(ctx, textData)
+	u, err := s.getUserFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = s.tRep.DeleteTextData(ctx, textData, u)
 	if err != nil {
 		return err
 	}
@@ -86,7 +99,12 @@ func (s *Server) DeleteTextData(ctx context.Context, textData storage.TextData) 
 }
 
 func (s *Server) AddBankData(ctx context.Context, bankData storage.BankData) (int, error) {
-	id, err := s.bankRep.AddBankData(ctx, bankData)
+	u, err := s.getUserFromContext(ctx)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := s.bankRep.AddBankData(ctx, bankData, u)
 	if err != nil {
 		return 0, err
 	}
@@ -94,7 +112,12 @@ func (s *Server) AddBankData(ctx context.Context, bankData storage.BankData) (in
 }
 
 func (s *Server) UpdateBankData(ctx context.Context, bankData storage.BankData) error {
-	err := s.bankRep.UpdateBankData(ctx, bankData)
+	u, err := s.getUserFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = s.bankRep.UpdateBankData(ctx, bankData, u)
 	if err != nil {
 		return err
 	}
@@ -102,7 +125,12 @@ func (s *Server) UpdateBankData(ctx context.Context, bankData storage.BankData) 
 }
 
 func (s *Server) DeleteBankData(ctx context.Context, bankData storage.BankData) error {
-	err := s.bankRep.DeleteBankData(ctx, bankData)
+	u, err := s.getUserFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = s.bankRep.DeleteBankData(ctx, bankData, u)
 	if err != nil {
 		return err
 	}
@@ -110,7 +138,12 @@ func (s *Server) DeleteBankData(ctx context.Context, bankData storage.BankData) 
 }
 
 func (s *Server) AddBinaryData(ctx context.Context, binaryData storage.BinaryData) (int, error) {
-	id, err := s.binRep.AddBinaryData(ctx, binaryData)
+	u, err := s.getUserFromContext(ctx)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := s.binRep.AddBinaryData(ctx, binaryData, u)
 	if err != nil {
 		return 0, err
 	}
@@ -118,7 +151,12 @@ func (s *Server) AddBinaryData(ctx context.Context, binaryData storage.BinaryDat
 }
 
 func (s *Server) UpdateBinaryData(ctx context.Context, binaryData storage.BinaryData) error {
-	err := s.binRep.UpdateBinaryData(ctx, binaryData)
+	u, err := s.getUserFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = s.binRep.UpdateBinaryData(ctx, binaryData, u)
 	if err != nil {
 		return err
 	}
@@ -126,7 +164,12 @@ func (s *Server) UpdateBinaryData(ctx context.Context, binaryData storage.Binary
 }
 
 func (s *Server) DeleteBinaryData(ctx context.Context, binaryData storage.BinaryData) error {
-	err := s.binRep.DeleteBinaryData(ctx, binaryData)
+	u, err := s.getUserFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = s.binRep.DeleteBinaryData(ctx, binaryData, u)
 	if err != nil {
 		return err
 	}
@@ -134,17 +177,21 @@ func (s *Server) DeleteBinaryData(ctx context.Context, binaryData storage.Binary
 }
 
 func (s *Server) GetAllRecords(ctx context.Context) (*storage.RecordsList, error) {
-	var err error
+	u, err := s.getUserFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
 	recList := &storage.RecordsList{}
-	recList.TextDataList, err = s.tRep.GetAllRecords(ctx)
+	recList.TextDataList, err = s.tRep.GetAllRecords(ctx, u)
 	if err != nil {
 		return nil, err
 	}
-	recList.BankDataList, err = s.bankRep.GetAllRecords(ctx)
+	recList.BankDataList, err = s.bankRep.GetAllRecords(ctx, u)
 	if err != nil {
 		return nil, err
 	}
-	recList.BinaryDataList, err = s.binRep.GetAllRecords(ctx)
+	recList.BinaryDataList, err = s.binRep.GetAllRecords(ctx, u)
 	if err != nil {
 		return nil, err
 	}
