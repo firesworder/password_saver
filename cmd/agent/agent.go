9e87f6fd0c246560b8ef79e61ff85b2097110fda
firesworder <cus.never.again@yandex.ru>
2023-09-08 19:37:35 +0300

    Добавил вывод версии программы

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/firesworder/password_saver/internal/agent"
 	"github.com/firesworder/password_saver/internal/agent/env"
 	"log"
 )
 
+var (
+	buildVersion = "0.0.1"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
+
 	env.ParseEnvArgs()
 
 	a, err := agent.NewAgent(&env.Env)
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/firesworder/password_saver/internal/grpcserver"
 	"github.com/firesworder/password_saver/internal/server"
 	"github.com/firesworder/password_saver/internal/server/env"
 	"log"
 )
 
+var (
+	buildVersion = "0.0.1"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
+
 	env.ParseEnvArgs()
 
 	s, err := server.NewServer()
