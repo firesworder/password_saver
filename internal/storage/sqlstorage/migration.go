bb278b9023f342ee86f147d210ffa1e4da072e03
firesworder <cus.never.again@yandex.ru>
2023-09-28 18:04:25 +0400

    server_storage_fix1 Изменил представление записей в БД

--- internal/storage/sqlstorage/migration.go
+++ internal/storage/sqlstorage/migration.go
@@ -8,27 +8,12 @@ CREATE TABLE IF NOT EXISTS Users
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS TextData
+CREATE TABLE IF NOT EXISTS Records
 (
     id SERIAL PRIMARY KEY,
-    text_data BYTEA,
-    meta_info TEXT,
-	user_id INTEGER REFERENCES Users(id)
-);
-
-CREATE TABLE IF NOT EXISTS BankData
-(
-    id SERIAL PRIMARY KEY,
-    bank_data BYTEA,
-    meta_info TEXT,
-	user_id INTEGER REFERENCES Users(id)
-);
-
-CREATE TABLE IF NOT EXISTS BinaryData
-(
-    id SERIAL PRIMARY KEY,
-    binary_data BYTEA,
-    meta_info TEXT,
-	user_id INTEGER REFERENCES Users(id)
+    content BYTEA,
+    met_info TEXT,
+    type VARCHAR(30),
+    user_id INTEGER REFERENCES Users(id)
 );
 `
